05afa4c96cf3801eb8895d5c6d4761c58309bf2a
Melwyna <47457927+Melwyna@users.noreply.github.com>
2019-02-23 10:38:55 -0500

    Mi primera modificacion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-a = int(input("inserte el primer numero"))
-c = int(input("inserte el segundo numero"))
-producto= (a*c)
-doble= (a*2)
-print ("el producto de los dos numeros es:", producto, "y el doble del primer numero es:", doble )
+import math
+b = int(input("inserte el numero para calcular el cuadrado de este"))
+d = float(input("inserte el numero para calcular la raiz cuadrada de este"))
+cuadrado= (b*b)
+raiz= math.sqrt(d)
+print ("el cuadrado del primer numero es:", cuadrado, ",y la raiz cuadrada del segundo numero es:", raiz)
