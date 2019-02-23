5b93dabe04e41e5f887bfcdd6f8992c3b73ab0a9
Melwyna <47457927+Melwyna@users.noreply.github.com>
2019-02-23 13:45:02 -0500

    Solucion

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import math
-b = int(input("inserte el numero para calcular el cuadrado de este"))
-d = float(input("inserte el numero para calcular la raiz cuadrada de este"))
-cuadrado= (b*b)
-raiz= math.sqrt(d)
-print ("el cuadrado del primer numero es:", cuadrado, ",y la raiz cuadrada del segundo numero es:", raiz)
+a = int(input("inserte el primer numero (a) porfavor:"))
+b = int(input("inserte el segundo numero (b) porfavor:"))
+c = int(input("inserte el tercer numero (c) porfavor:"))
+d =math.sqrt((b**2)-(4*a*c))
+x_pos = (-b + d)/(2 * a)
+x_neg = (-b - d)/(2 * a)
+cu_0 = -b/(2*a)
+if d > 0:
+ print ("La parte negativa es:"+ str(x_pos))
+ print ("La parte positiva es:"+ str(x_neg))
+if d == 0:
+  print ("la solucion es:"+ str(cu_0))
+if d < 0:
+ print ("No existe solución en los números reales")
